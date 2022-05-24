066c163128b64058bffd9f67714920d759aa2e9f
Madalin Balint <madalin_bacau@yahoo.com>
2022-05-24 08:42:07 +0300

    added DI support for repository

--- app/src/main/java/com/mendelin/flickerapp/di/DatabaseModule.kt
+++ app/src/main/java/com/mendelin/flickerapp/di/DatabaseModule.kt
@@ -1,7 +1,9 @@
 package com.mendelin.flickerapp.di
 
 import com.mendelin.flickerapp.data.remote.FlickrApi
+import com.mendelin.flickerapp.data.repository.FlickrRepositoryImpl
 import com.mendelin.flickerapp.domain.remote.RetrofitServiceProvider
+import com.mendelin.flickerapp.domain.repository.FlickrRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -11,7 +13,10 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
     @Provides
-    fun provideFlickrDataSource(): FlickrApi {
-        return RetrofitServiceProvider.getService()
-    }
+    fun provideFlickrApi(): FlickrApi =
+        RetrofitServiceProvider.getService()
+
+    @Provides
+    fun provideFlickrRepositoryImpl(service: FlickrApi): FlickrRepository =
+        FlickrRepositoryImpl(service)
 }
